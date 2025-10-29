1fea0cbb0da820409b53a38203c466b172df1344
luisito99-bit <82012568+luisito99-bit@users.noreply.github.com>
2025-10-29 17:14:48 -0600

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,56 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "I remember when I first noticed", time: 41 },
+  { text: "that you liked me back", time: 42 },
+  { text: "We were sitting down in  ", time: 52 },
+  { text: "a restaurant waiting for the check", time: 54 },
+  { text: "We had made love earlier that day with no strings attached", time:63 },
+  { text:"But I could tell that something had changed how you looked at me then", time:74},
+  { text:"Kristen, come right back", time:83},
+  { text:"I've been waiting for you", time:85},
+  { text:"To slip back in bed", time:89},
+  { text:"When you light the candle", time:91},
+];
+
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
